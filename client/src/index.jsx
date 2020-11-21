1af43b03274d4bccdb6bcb85955ab1d360a9e757
markwgquintos <markwgquintos@yahoo.com>
2020-11-20 19:05:15 -0800

    changed react render path

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './compenents/App.jsx'
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('morePlaces'));
